f52a025cf1e07d7f2a01549aeaa184505aff64fa
LittleBird <ilms49898723@gmail.com>
2016-05-23 23:59:24 +0900

    update

--- client.cpp
+++ client.cpp
@@ -1,12 +1,23 @@
-#include <bits/stdc++.h>
+#include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#include <string>
+#include <deque>
+#include <vector>
+#include <map>
+#include <utility>
 #include <thread>
 #include "Socket.hpp"
+#include "npinc.h"
 
 int main(const int argc, const char** argv) {
     if (argc != 3) {
         fprintf(stderr, "usage %s <server address> <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    int port;
+    if (sscanf(argv[2], "%d", &port) != 1) {
+        fprintf(stderr, "Invalid port number\n");
         return EXIT_FAILURE;
     }
     return 0;
--- server.cpp
+++ server.cpp
@@ -1,12 +1,23 @@
-#include <bits/stdc++.h>
+#include <cstdio>
 #include <cstring>
 #include <cstdlib>
+#include <string>
+#include <deque>
+#include <vector>
+#include <map>
+#include <utility>
 #include <thread>
 #include "Socket.hpp"
+#include "npinc.h"
 
 int main(const int argc, const char** argv) {
     if (argc != 2) {
         fprintf(stderr, "usage %s <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    int port;
+    if (sscanf(argv[1], "%d", &port) != 1) {
+        fprintf(stderr, "Invalid port number\n");
         return EXIT_FAILURE;
     }
     return 0;
