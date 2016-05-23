1a0ba280b5123dc24b62291b90fe549b1a43d6e2
LittleBird <ilms49898723@gmail.com>
2016-05-23 20:42:20 +0900

    Update

--- client.cpp
+++ client.cpp
@@ -4,10 +4,11 @@
 #include <thread>
 #include "Socket.hpp"
 
-int main(const int& argc, const char** argv) {
+int main(const int argc, const char** argv) {
     if (argc != 3) {
         fprintf(stderr, "usage %s <server address> <port>\n", argv[0]);
         return EXIT_FAILURE;
     }
     return 0;
 }
+
--- server.cpp
+++ server.cpp
@@ -4,10 +4,11 @@
 #include <thread>
 #include "Socket.hpp"
 
-int main(const int& argc, const char** argv) {
+int main(const int argc, const char** argv) {
     if (argc != 2) {
         fprintf(stderr, "usage %s <port>\n", argv[0]);
         return EXIT_FAILURE;
     }
     return 0;
 }
+
