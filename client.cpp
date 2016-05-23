bbaf20d4cfec90fbd72c777e81bf2631de2d3e45
LittleBird <ilms49898723@gmail.com>
2016-05-23 20:30:37 +0900

    Initial commit

---
+++ client.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <cstdlib>
+#include <thread>
+#include "Socket.hpp"
+
+int main(const int& argc, const char** argv) {
+    if (argc != 3) {
+        fprintf(stderr, "usage %s <server address> <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    return 0;
+}
---
+++ server.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <cstdlib>
+#include <thread>
+#include "Socket.hpp"
+
+int main(const int& argc, const char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "usage %s <port>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    return 0;
+}
