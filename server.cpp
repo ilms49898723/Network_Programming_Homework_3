3f2b1f3351ba5452d55cb92ee7a5915b35810958
LittleBird <ilms49898723@gmail.com>
2016-05-24 14:09:35 +0900

    add thread functions

--- server.cpp
+++ server.cpp
@@ -2,6 +2,7 @@
 #include <cstring>
 #include <cstdlib>
 #include <string>
+#include <sstream>
 #include <deque>
 #include <vector>
 #include <map>
@@ -11,26 +12,66 @@
 #include <chrono>
 #include "npinc.h"
 
-std::vector<std::pair<std::thread, bool>> threads;
+// mutex
+struct Lockers {
+    std::mutex accountLocker;
+};
 
-void threadMaintainer();
-void serverFunc(int fd);
+// account information
+struct Account {
+    std::string account;
+    std::string password;
+    bool isOnline;
+};
+
+// server utility
+class ServerUtility {
+public:
+    ServerUtility(int fd) {
+        this->fd = fd;
+    }
+
+    ~ServerUtility() {
+
+    }
+
+private:
+    Lockers lockers;
+    int fd;
+};
+
+// user data
+std::map<std::string, Account> userData;
+
+// vector of threads
+std::vector<std::pair<std::thread, bool>> threads;
+// thread related functions
+void threadMaintain();
+void endThread();
+std::string getThreadInfo();
+
+// server init functions
 int parseArgument(int argc, const char** argv);
 int newServer(const int& port);
 std::pair<sockaddr_in, int> newConnection(const int& listenfd);
+std::pair<std::string, int> getConnectionInfo(const sockaddr_in sock);
+
+// server main function
+void serverFunc(int fd, std::pair<std::string, int> connectInfo);
 
 int main(int argc, const char** argv) {
     int port = parseArgument(argc, argv);
     int listenfd = newServer(port);
-    threads.push_back(std::make_pair(std::thread(threadMaintainer), true));
+    threads.push_back(std::make_pair(std::thread(threadMaintain), true));
     while (true) {
         std::pair<sockaddr_in, int> client = newConnection(listenfd);
-        threads.push_back(std::make_pair(std::thread(serverFunc, client.second), true));
+        std::pair<std::string, int> connectInfo = getConnectionInfo(client.first);
+        threads.push_back(std::make_pair(std::thread(serverFunc, client.second, connectInfo), true));
     }
     return 0;
 }
 
-void threadMaintainer() {
+void threadMaintain() {
     while (true) {
         bool flag = false;
         unsigned toRemove = 0xFFFFFFFFu;
@@ -41,7 +82,7 @@ void threadMaintainer() {
             }
         }
         if (flag) {
-            printf("Remove thread #%d(finished)\n", toRemove);
+            printf("Thread #%d finished. Removed.\n", toRemove);
             threads.at(toRemove).first.join();
             threads.erase(threads.begin() + toRemove);
         }
@@ -49,33 +90,19 @@ void threadMaintainer() {
     }
 }
 
-void serverFunc(int fd) {
-    char buffer[MAXN];
-    while (true) {
-        int n;
-        if ((n = read(fd, buffer, MAXN)) == 0) {
-            break;
-        }
-        if (n < 0) {
-            fprintf(stderr, "%s", strerror(errno));
-            break;
-        }
-        printf("%s", buffer);
-        if ((n = write(fd, buffer, MAXN)) == 0) {
-            break;
-        }
-        if (n < 0) {
-            fprintf(stderr, "%s", strerror(errno));
+void endThread() {
+    for (auto&& item : threads) {
+        if (std::this_thread::get_id() == item.first.get_id()) {
+            item.second = false;
             break;
         }
     }
-    for (auto&& i : threads) {
-        if (std::this_thread::get_id() == i.first.get_id()) {
-            printf("Marked\n");
-            i.second = false;
-            break;
-        }
-    }
+}
+
+std::string getThreadInfo() {
+    std::ostringstream oss;
+    oss << std::hex << std::this_thread::get_id();
+    return oss.str();
 }
 
 int parseArgument(int argc, const char** argv) {
@@ -121,3 +148,36 @@ std::pair<sockaddr_in, int> newConnection(const int& listenfd) {
     return std::make_pair(client, clientfd);
 }
 
+std::pair<std::string, int> getConnectionInfo(const sockaddr_in sock) {
+    std::string address = inet_ntoa(sock.sin_addr);
+    int port = ntohs(sock.sin_port);
+    return std::make_pair(address, port);
+}
+
+void serverFunc(int fd, std::pair<std::string, int> connectInfo) {
+    printf("New thread id %s started\n", getThreadInfo().c_str());
+    printf("New connection from %s port %d\n", connectInfo.first.c_str(), connectInfo.second);
+    ServerUtility serverUtility(fd);
+    char buffer[MAXN];
+    while (true) {
+        int n;
+        if ((n = read(fd, buffer, MAXN)) == 0) {
+            break;
+        }
+        if (n < 0) {
+            fprintf(stderr, "%s", strerror(errno));
+            break;
+        }
+        printf("%s", buffer);
+        if ((n = write(fd, buffer, MAXN)) == 0) {
+            break;
+        }
+        if (n < 0) {
+            fprintf(stderr, "%s", strerror(errno));
+            break;
+        }
+    }
+    endThread();
+    printf("Thread id %s finished\n", getThreadInfo().c_str());
+    printf("%s port %d disconnected\n", connectInfo.first.c_str(), connectInfo.second);
+}
