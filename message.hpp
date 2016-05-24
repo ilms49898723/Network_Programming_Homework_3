e9c8d197a15cfebe26df48f13a9ca446c04b6298
LittleBird <ilms49898723@gmail.com>
2016-05-24 21:25:13 +0900

    Initial commit

---
+++ message.hpp
@@ -0,0 +1,15 @@
+#ifndef NETWORK_PROGRAMMING_MESSAGE_HPP_
+#define NETWORK_PROGRAMMING_MESSAGE_HPP_
+
+#include <string>
+
+const std::string msgNEWCONNECTION = "NEWCONNECTION";
+const std::string msgREGISTER = "REGISTER";
+const std::string msgLOGIN = "LOGIN";
+const std::string msgLOGOUT = "LOGOUT";
+const std::string msgDELETEACCOUNT = "DELETEACCOUNT";
+
+const std::string msgSUCCESS = "SUCCESS";
+const std::string msgFAIL = "FAIL";
+
+#endif // NETWORK_PROGRAMMING_MESSAGE_HPP_
